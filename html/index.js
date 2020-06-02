2bcee454d61104417d83dda38a238d2cad760e79
shoHasegawa28 <b1601732@planet.kanazawa-it.ac.jp>
2020-06-02 20:26:06 +0900

    html削除

--- html/index.js
+++
@@ -1,8 +0,0 @@
-function clickTest() {
-      target = document.getElementById("anime_test");
-      if (target.className == null || target.className=="") {
-        target.className = "active2";
-      } else {
-        target.className = "";
-      }
-    }
